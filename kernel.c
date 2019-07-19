2d16cc1dec67aade2e06dfb2f414553245719b10
Li Xu <jcuwundt@gmail.com>
2019-07-19 10:21:17 +1000

    ..

--- kernel.c
+++ kernel.c
@@ -1,10 +1,21 @@
-kmain()
-{
-        char* vidmem=(char*)0xb8000;
-        char* text = "hey mate, how are you going?";
 
-        for(int i = 0; i < 29 ; i++) {
-          vidmem[i*2] = text[i];
-          vidmem[i*2+1] = 0x02;
-        }
+#define VIDEO_MEMORY_START_POINT 0xB8000 
+
+void write_string(int colour, const char *string);
+
+void kmain()
+{
+    int colour = 0x04;
+    write_string(0x04, "G'day mate, how are you going?");
+}
+
+
+void write_string( int colour, const char *string )
+{
+    volatile char *video = (volatile char*)VIDEO_MEMORY_START_POINT;
+    while( *string != 0 )
+    {
+        *video++ = *string++;
+        *video++ = colour;
+    }
 }
